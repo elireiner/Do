c5dc64ea99a1a2d0df1e5c02d5ae881ee4393713
Eli Reiners <goelyukim@gmail.com>
2020-02-20 10:17:52 -0500

    Add components and some event handler code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import AddForm from './AddForm/AddForm'
+import DoList from './DoList/DoList'
 
 class App extends Component {
+  state = {
+    toDos: []
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const toDo = {name: e.target.add.value}
+    this.setState({toDos: this.state.toDos.push(toDo) })
+    console.log(this.state.toDos)
+}
   render() {
     return (
-      <main className='App'>
-        {/* content goes here */}
-      </main>
+      <>
+      <AddForm handleSubmit={this.handleSubmit}/>
+      <DoList/>
+      </>
     )
   }
 }
