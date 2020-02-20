8795dd5707fe1162394791a5b1a119e1a611d80a
Eli Reiners <goelyukim@gmail.com>
2020-02-20 10:19:28 -0500

    Add basic code

---
+++ src/DoList/DoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DoList extends Component{
+    render(){
+        return(
+            <main>
+                hi
+            </main>
+        )
+    }
+}
+
+export default DoList
