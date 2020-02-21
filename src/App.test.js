da22ed9bb0b03cb34f487487ae31e4a8b0a17c48
Eli Reiners <goelyukim@gmail.com>
2020-02-21 13:54:30 -0500

    Add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 
@@ -8,6 +9,10 @@ describe('App', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div)
     ReactDOM.unmountComponentAtNode(div)
+  })
+  it('renders UI as expected', () => {
+    const tree = renderer.create(<App/>).toJSON();
+    expect(tree).toMatchSnapshot();
   })
 })
 
