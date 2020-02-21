83175fe4895501626ee221d9a09f5bde360e9f2e
Eli Reiners <goelyukim@gmail.com>
2020-02-20 21:39:59 -0500

    Correct typo

--- src/AddForm/AddForm.test.js
+++ src/AddForm/AddForm.test.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import AddForm from './AddForm';
+import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme'
 
 describe('AddForm', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<AddFrom />, div);
+        ReactDOM.render(<AddForm />, div);
         ReactDOM.unmountComponentAtNode(div)
     })
     it('renderer UI as expected', () => {
         const tree = renderer.create(<AddForm/>).toJSON();
-        expect(tree).toMatchSnapShot()
+        expect(tree).toMatchSnapShot
     })
     it('calls handleSubmit when button is clicked', () => {
         const mockFunction = jest.fn()
         const wrapper = shallow(<AddForm handleSubmit={mockFunction}/>);
-        wrapper.find('button').at('one').simulate('click');
+        wrapper.find('button').simulate('click');
         expect(mockFunction).toHaveBeenCalled();
     })
 })
