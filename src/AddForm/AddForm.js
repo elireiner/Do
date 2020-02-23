53b0a5432a34fa2646becc50bbc8503ba758c219
Eli Reiners <goelyukim@gmail.com>
2020-02-23 15:13:38 -0500

    Change to context

--- src/AddForm/AddForm.js
+++ src/AddForm/AddForm.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Context from '../Context'
 
 export default class AddForm extends Component{
 
+    static contextType = Context;
+
     render(){
         return(
-            <form onSubmit={this.props.handleSubmit}>
+            <form onSubmit={this.context.handleSubmit}>
                 <label>
                     Add to do:
                     <input name="add"/>
