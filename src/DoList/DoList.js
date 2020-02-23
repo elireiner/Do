38e544f1244bb73e11c26f674e5374ccef17e8ec
Eli Reiners <goelyukim@gmail.com>
2020-02-23 15:13:53 -0500

    Add context and link

--- src/DoList/DoList.js
+++ src/DoList/DoList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ToDo from '../ToDo/ToDo';
+import Context from '../Context'
 
 class DoList extends Component {
     static defaultProps = {
         ToDoList: []
     }
+
+    static contextType = Context;
+
     render() {
+
+        const { ToDoList } = this.context
+
         return (
+            <>
             <ul>
-                {this.props.ToDoList.map((item) => (<ToDo name={item.name} />))}
+                {ToDoList.map((item) => (<ToDo name={item.name} />))}
             </ul>
+            <Link to={`/AddForm`}>Add ToDo</Link>
+            </>
         )
     }
 }
